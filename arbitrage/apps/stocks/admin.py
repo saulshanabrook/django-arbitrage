416a9da5912007ac34f7692593d15cba6c0850b8
Saul Shanabrook <s.shanabrook@gmail.com>
2012-09-08 04:15:09 -0400

    Improved admin and added initial data

--- arbitrage/apps/stocks/admin.py
+++ arbitrage/apps/stocks/admin.py
@@ -18,6 +18,16 @@ class StockGroupInline(admin.TabularInline):
 
 class GroupAdmin(admin.ModelAdmin):
     inlines = (StockGroupInline,)
+    list_display = ['name', 'completion_date', 'greatest_difference', 'apr']
+    actions = ['sync_stocks']
+
+    def sync_stocks(self, request, queryset):
+        message_bits = []
+        for group in queryset:
+            [stock.sync() for stock in group.stocks.all()]
+            message_bits.append("{} stocks updated in {}".format(group.stocks.count(),
+                                                            group))
+        self.message_user(request, '\n'.join(message_bits))
 
 admin.site.register(Stock, StockAdmin)
 admin.site.register(Group, GroupAdmin)
